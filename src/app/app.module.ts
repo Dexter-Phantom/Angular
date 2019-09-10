5905d45f2c62e093f078b1c66f3ccc66d064fba8
NINJ7 <creamfields44@gmail.com>
2019-09-10 10:04:31 -0500

    Page no found component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
+    NopagefoundComponent,
   ],
   imports: [
     BrowserModule,
