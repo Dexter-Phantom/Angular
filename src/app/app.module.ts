a23bc2ecae29baf54c20a38dd4a94cc7e45bc24a
NINJ7 <creamfields44@gmail.com>
2019-09-10 10:05:48 -0500

    Dashboard component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     NopagefoundComponent,
+    DashboardComponent,
   ],
   imports: [
     BrowserModule,
