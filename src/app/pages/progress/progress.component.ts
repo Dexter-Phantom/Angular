bb3d1d2abcd7f77b89f2197ebeb0a7430f40dada
NINJ7 <creamfields44@gmail.com>
2019-09-13 17:16:30 -0500

    Loading progress

--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -3,13 +3,30 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-progress',
   templateUrl: './progress.component.html',
-  styles: []
+  styles: ['.md-taps {margin-left: 25px;}']
 })
 export class ProgressComponent implements OnInit {
+
+  percent: number = 0;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+
+  changeValue = (valor) => {
+    if (this.percent >= 100 && valor > 0) {
+       this.percent = 100;
+       return;
+    }
+    if (this.percent <= 0 && valor < 0) {
+      this.percent = 0;
+      return;
+    }
+
+    this.percent = this.percent + valor;
+    return;
   }
 
 }
